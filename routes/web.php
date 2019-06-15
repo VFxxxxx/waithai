78a4632d01a43d275b67832e01363b5b30a27975
Vladimir <fedotow.vla2017@yandex.ru>
2019-06-15 17:44:15 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Authentication Routes...
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::match(['get', 'post' ], 'logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::group(['prefix' => App\Http\Middleware\LocaleMiddleware::getLocale()], function(){
 
     Route::get('setlocale/{lang}', 'LocaleController@setLocale')->name('setlocale');
-
-    // Authentication Routes...
-    Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-    Route::post('login', 'Auth\LoginController@login');
-    Route::match(['get', 'post' ], 'logout', 'Auth\LoginController@logout')->name('logout');
-
-
 
     Route::group(['prefix' => 'admin'], function(){
         Route::group(['middleware' => ['auth']], function () {
